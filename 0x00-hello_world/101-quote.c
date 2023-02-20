334ce1c092e0d248b6ece4933ce5ddad63afbba6
Lehlohonolo396 <tlhogi928@gmai.com>
2023-02-20 11:43:36 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Printing the line of a C program error
+ * Return 1
+ */
+int mai(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
