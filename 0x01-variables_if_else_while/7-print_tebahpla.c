be097d3375392edc4cfc610f25ce68366f2128a7
Lehlohonolo396 <tlhogi928@gmai.com>
2023-02-20 17:50:11 -0800

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Print the alphabet reversed using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
